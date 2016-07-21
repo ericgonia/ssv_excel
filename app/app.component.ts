c925e5cdc933e045d4e9b092d79b52ced1758487
Eric Gonia <eric.gonia@gmail.com>
2016-07-20 21:02:12 -0500

    Beginnings.

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { HTTP_PROVIDERS } from '@angular/http';
+//import { Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
+//import 'rxjs/Rx'; // load the full rxjs
+
+@Component({
+    //moduleId: module.id,
+    selector: 'ssv_excel',
+    templateUrl: 'app.component.html',
+})
+
+export class AppComponent {}
